39330b314a085865a94c1ffe3f2fb68a172b729f
Rae <raquel.fontes28@icloud.com>
2024-11-11 19:54:32 -0300

    update: notion page id 2

--- site.config.js
+++ site.config.js
@@ -32,7 +32,7 @@ const CONFIG = {
   // notion configuration (required)
   notionConfig: {
     //pageId: process.env.NOTION_PAGE_ID,
-    pageId: "13b4f4c3dbcf80f4b133ed446907862f",
+    pageId: "1334f4c3dbcf80899321c6427064d8a8",
   },
 
   // plugin configuration (optional)
